e86b232d248669fd12d797aaae4d26533253eba8
Joshua Martinez <joshkmartinez@gmail.com>
2018-06-23 19:35:19 -0700

    set up some basic auth

--- app/screens/login.js
+++ app/screens/login.js
@@ -4,20 +4,53 @@ import {
   Image,
   TextInput,
   StyleSheet,
+  Alert,
   KeyboardAvoidingView,
   Dimensions
 } from "react-native";
 import { Button, Text } from "native-base";
 
-const width = Dimensions.get("window").width;
-const height = Dimensions.get("window").height;
+let width = Dimensions.get("window").width;
+let height = Dimensions.get("window").height;
 
 export default class Login extends React.Component {
   static navigationOptions = {
     title: "Login to Aeries"
   };
 
+  constructor(props) {
+    super(props);
+    this.state = { isAuthed: false };
+    this.state = { username: "" };
+    this.state = { authError: false };
+    this.state = { password: "" };
+
+    auth = () => {
+
+      //do auth process
+      this.setState({ authError: false });
+      this.setState({ isAuthed: true });
+      if (isAuthed === false) { // auth error
+        this.setState({ authError: true });
+        this.showAlert();
+      }
+      if (isAuthed === true) { // nav to grades screen is authentication is successful
+        // save auth credentials
+        this.props.navigation.navigate("Grades")
+      }
+
+
+    }
+
+    const showAlert = () => {
+      Alert.alert(
+         'There was an error loging in. Please check your username and password and try again.'
+      )
+   }
+
+  }
   render() {
+    const {showAlert} = this.state
     return (
       <View style={styles.wrapper}>
         <KeyboardAvoidingView behavior="padding" style={styles.loginContainer}>
@@ -26,6 +59,7 @@ export default class Login extends React.Component {
             keyboardType="email-address"
             placeholderTextColor="white"
             style={styles.input}
+            value={this.state.username}
           />
 
           <TextInput
@@ -33,18 +67,21 @@ export default class Login extends React.Component {
             secureTextEntry
             placeholderTextColor="white"
             style={styles.input}
+            value={this.state.password}
           />
 
           <View style={{ alignItems: "center" }}>
             <Button
               primary
-              onPress={() => this.props.navigation.navigate("Grades")}
+              onPress={showAlert}
               style={styles.loginButton}
             >
               <Text> Login </Text>
             </Button>
+
           </View>
         </KeyboardAvoidingView>
+       
       </View>
     );
   }
@@ -89,5 +126,9 @@ const styles = {
     borderRadius: 3,
     marginTop: 9,
     marginBottom: 21
+  },
+  error: {
+    color: 'red',
+    textAlign: 'center',
   }
 };
