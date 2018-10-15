18a11c6ce785e86730fe7c324ee23ce127b0c8d8
Joshua Martinez <joshkmartinez@gmail.com>
2018-10-15 00:13:31 -0700

    created a new class card with <FlatList/>

--- app/screens/grades.js
+++ app/screens/grades.js
@@ -7,7 +7,8 @@ import {
   Dimensions,
   AsyncStorage,
   ActivityIndicator,
-  BackHandler
+  BackHandler,
+  FlatList
 } from "react-native";
 import { withNavigation } from "react-navigation";
 import PTRView from "react-native-pull-to-refresh";
@@ -20,7 +21,7 @@ import {
   BottomNavigation,
   Surface,
   Button,
-  List,
+  //List,
   Headline,
   Switch,
   Divider,
@@ -28,98 +29,15 @@ import {
   Dialog,
   Portal
 } from "react-native-paper";
+import { List, ListItem } from "react-native-elements";
 import { Home } from "../components/";
 import { _ } from "lodash";
 let width = Dimensions.get("window").width;
 let height = Dimensions.get("window").height;
 
-const HomeRoute = () => (
-  <PTRView onRefresh={() => this.refresh}>
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Card name="Calculus BC" color="#d50000" letterGrade="A+" grade="99%" />
-      <Card name="Calculus BC" color="#ff6d00" letterGrade="A" grade="92%" />
-      <Card name="Calculus BC" color="#00c853" letterGrade="A+" grade="99%" />
-      <Card name="Calculus BC" color="#2962ff" letterGrade="B" grade="99%" />
-      <Card name="Calculus BC" color="#aa00ff" letterGrade="D+" grade="99%" />
-      <Card name="Calculus BC" color="#3e2723" letterGrade="C" grade="99%" />
-    </View>
-  </PTRView>
-);
-
-const Card = props => (
-  <Surface
-    style={{
-      margin: 3,
-      padding: 8,
-      backgroundColor: props.color,
-      elevation: 3,
-      justifyContent: "center",
-      width: width - 16,
-
-      height: height / 10 //divided by the number of classes you have, to a max of like 8, have a space at the bottom
-    }}
-  >
-    <View style={{ alignItems: "center", flexDirection: "row", flex: 1 }}>
-      <View
-        style={{
-          //backgroundColor: "blue",
-          height: 20,
-          width: width - width / 2 - 21,
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <Text
-          style={{
-            textAlign: "left",
-            alignSelf: "stretch",
-            color: "white",
-            fontSize: responsiveFontSize(3.3)
-          }}
-        >
-          {props.name}
-        </Text>
-      </View>
-      <View
-        style={{
-          //backgroundColor: "red",
-          height: 20,
-          width: width - width / 2 - 20,
-          flexDirection: "column",
-          justifyContent: "center"
-        }}
-      >
-        <Text
-          style={{
-            textAlign: "right",
-            //alignSelf: "stretch",
-            color: "white",
-            fontSize: responsiveFontSize(2.8)
-          }}
-        >
-          {props.grade}
-        </Text>
-        <Text
-          style={{
-            textAlign: "right",
-            //alignSelf: "stretch",
-            color: "white",
-            fontSize: responsiveFontSize(2.8)
-          }}
-        >
-          {props.letterGrade}
-        </Text>
-      </View>
-    </View>
-  </Surface>
-);
-
-const responsiveFontSize = f => {
-  return Math.sqrt(height * height + width * width) * (f / 100);
-};
 export class Grades extends React.Component {
   static navigationOptions = {
-    title: "Grades", //no title for now
+    title: "Grades",
     headerLeft: null,
     headerRight: <Button icon="settings" />,
     gesturesEnabled: false
@@ -128,7 +46,9 @@ export class Grades extends React.Component {
     super();
     this.state = {
       index: 0,
-      isAuthed: false
+      isAuthed: false,
+      data: [],
+      loading: false
     };
   }
 
@@ -200,4 +120,101 @@ export class Grades extends React.Component {
     );
   }
 }
+const HomeRoute = () => (
+  /*
+  <PTRView onRefresh={() => this.refresh}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Card name="Calculus BC" color="#d50000" letterGrade="A+" grade="99%" />
+      <Card name="Calculus BC" color="#ff6d00" letterGrade="A" grade="92%" />
+      <Card name="Calculus BC" color="#00c853" letterGrade="A+" grade="99%" />
+      <Card name="Calculus BC" color="#2962ff" letterGrade="B" grade="99%" />
+      <Card name="Calculus BC" color="#aa00ff" letterGrade="D+" grade="99%" />
+      <Card name="Calculus BC" color="#3e2723" letterGrade="C" grade="99%" />
+    </View>
+  </PTRView>*/ <List
+  >
+    <FlatList
+      data={this.state.data}
+      renderItem={({ item }) => (
+        <Card
+          name={item.name}
+          letterGrade={item.letterGrade}
+          grade={item.grade}
+        />
+      )}
+    />
+  </List>
+);
+
+const Card = props => (
+  <Surface
+    style={{
+      margin: 3,
+      padding: 6,
+      backgroundColor: props.color,
+      //elevation: 3,
+      justifyContent: "center",
+      width: width - 16,
+
+      height: height / 9 //divided by the number of classes you have, to a max of like 8, have a space at the bottom
+    }}
+  >
+    <View style={{ alignItems: "center", flexDirection: "row", flex: 1 }}>
+      <View
+        style={{
+          //backgroundColor: "blue",
+          //height: 20,
+          width: width - width / 2 - 21,
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Text
+          style={{
+            textAlign: "left",
+            //alignSelf: "stretch",
+            color: "white",
+            fontSize: responsiveFontSize(3)
+          }}
+        >
+          {props.name}
+        </Text>
+      </View>
+      <View
+        style={{
+          //backgroundColor: "red",
+          //height: 20,
+          width: width - width / 2 - 20,
+          flexDirection: "column",
+          justifyContent: "center"
+        }}
+      >
+        <Text
+          style={{
+            textAlign: "right",
+            //alignSelf: "stretch",
+            color: "white",
+            fontSize: responsiveFontSize(2.6)
+          }}
+        >
+          {props.grade}
+        </Text>
+        <Text
+          style={{
+            textAlign: "right",
+            //alignSelf: "stretch",
+            color: "white",
+            fontSize: responsiveFontSize(2.6)
+          }}
+        >
+          {props.letterGrade}
+        </Text>
+      </View>
+    </View>
+  </Surface>
+);
+
+const responsiveFontSize = f => {
+  return Math.sqrt(height * height + width * width) * (f / 100);
+};
 export default withNavigation(Grades);
