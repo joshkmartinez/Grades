761b88331c91c2bc60c8f59c44eaa0aa6144e629
Joshua Martinez <joshkmartinez@gmail.com>
2018-10-15 22:22:23 -0700

    styling changes

--- app/screens/grades.js
+++ app/screens/grades.js
@@ -9,6 +9,7 @@ import {
   ActivityIndicator,
   BackHandler,
   FlatList,
+  SafeAreaView,
   TouchableOpacity
 } from "react-native";
 import { withNavigation } from "react-navigation";
@@ -142,55 +143,10 @@ export class Grades extends React.Component {
   };
   render() {
     return (
-      <Root>
-        <ScrollableTabView initialPage={0} tabBarPosition="top">
-          <HomeRoute tabLabel="Grades" data={this.state.data} />
-
-          <View
-            style={{ flex: 1, width: width - 9, justifyContent: "center" }}
-            tabLabel="Settings"
-          >
-            <SettingsList backgroundColor="transparent">
-              <SettingsList.Header
-                headerText="Settings"
-                headerStyle={{ color: "black" }}
-              />
-
-              <SettingsList.Item
-                itemWidth={50}
-                title="About/Credits"
-                onPress={() => Alert.alert("Go to credits website")}
-              />
-              <SettingsList.Item
-                itemWidth={50}
-                title="Help"
-                onPress={() => Alert.alert("Direct to help website")}
-              />
-              <SettingsList.Item
-                itemWidth={50}
-                title="Logout"
-                onPress={() =>
-                  Alert.alert(
-                    "Log out",
-                    "Do you want to log out?",
-                    [
-                      {
-                        text: "No",
-                        onPress: () => console.log("Cancel Pressed")
-                      },
-                      {
-                        text: "Yes",
-                        onPress: () => this.props.navigation.navigate("Login")
-                      }
-                    ],
-                    { cancelable: false }
-                  )
-                }
-              />
-            </SettingsList>
-            {/*make is so the saved username and password are cleared when logout*/}
-          </View>
-        </ScrollableTabView>
+      <Root
+        styles={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      >
+        <HomeRoute tabLabel="Grades" data={this.state.data} />
       </Root>
     );
   }
@@ -208,153 +164,144 @@ const HomeRoute = ({ data }) => (
       <Card name="Calculus BC" color="#3e2723" letterGrade="C" grade="99%" />
     </View>
   </PTRView>*/
-  <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0, flex: 1 }}>
-    <FlatList
-      style={{ width: width }}
-      data={data}
-      renderItem={({ item }) => (
-        <Surface
-          style={{
-            margin: 3,
-            padding: 6,
-            backgroundColor: item.color,
-            //elevation: 3,
-            justifyContent: "center",
-            width: width - 16,
-
-            height: height / 9 //divided by the number of classes you have, to a max of like 8, have a space at the bottom
-          }}
-        >
-          <View style={{ alignItems: "center", flexDirection: "row", flex: 1 }}>
-            <View
-              style={{
-                //backgroundColor: "blue",
-                //height: 20,
-                width: width - width / 2 - 21,
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            >
-              <Text
-                style={{
-                  textAlign: "left",
-                  //alignSelf: "stretch",
-                  color: "white",
-                  fontSize: responsiveFontSize(3)
-                }}
-              >
-                {item.name}
-              </Text>
-            </View>
-            <View
-              style={{
-                //backgroundColor: "red",
-                //height: 20,
-                width: width - width / 2 - 20,
-                flexDirection: "column",
-                justifyContent: "center"
-              }}
-            >
-              <Text
-                style={{
-                  textAlign: "right",
-                  //alignSelf: "stretch",
-                  color: "white",
-                  fontSize: responsiveFontSize(2.6)
-                }}
-              >
-                {item.grade}
-              </Text>
-              <Text
-                style={{
-                  textAlign: "right",
-                  //alignSelf: "stretch",
-                  color: "white",
-                  fontSize: responsiveFontSize(2.6)
-                }}
-              >
-                {item.letterGrade}
-              </Text>
-            </View>
-          </View>
-        </Surface>
-      )}
-      ItemSeparatorComponent={this.renderSeparator}
-      //ListHeaderComponent={this.renderHeader}
-      ListFooterComponent={this.renderFooter}
-      keyExtractor={item => item.name}
-    />
-  </List>
-);
-
-const Card = props => (
-  <Surface
-    style={{
-      margin: 3,
-      padding: 6,
-      backgroundColor: props.color,
-      //elevation: 3,
-      justifyContent: "center",
-      width: width - 16,
-
-      height: height / 9 //divided by the number of classes you have, to a max of like 8, have a space at the bottom
-    }}
+  <SafeAreaView
+    style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
   >
-    <View style={{ alignItems: "center", flexDirection: "row", flex: 1 }}>
-      <View
-        style={{
-          //backgroundColor: "blue",
-          //height: 20,
-          width: width - width / 2 - 21,
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <Text
-          style={{
-            textAlign: "left",
-            //alignSelf: "stretch",
-            color: "white",
-            fontSize: responsiveFontSize(3)
-          }}
-        >
-          {props.name}
-        </Text>
-      </View>
-      <View
-        style={{
-          //backgroundColor: "red",
-          //height: 20,
-          width: width - width / 2 - 20,
-          flexDirection: "column",
-          justifyContent: "center"
-        }}
-      >
-        <Text
-          style={{
-            textAlign: "right",
-            //alignSelf: "stretch",
-            color: "white",
-            fontSize: responsiveFontSize(2.6)
-          }}
-        >
-          {props.grade}
-        </Text>
-        <Text
-          style={{
-            textAlign: "right",
-            //alignSelf: "stretch",
-            color: "white",
-            fontSize: responsiveFontSize(2.6)
-          }}
-        >
-          {props.letterGrade}
-        </Text>
-      </View>
-    </View>
-  </Surface>
-);
+    <List
+      containerStyle={{
+        borderTopWidth: 0,
+        borderBottomWidth: 0,
+        marginTop: 6,
+        flex: 1,
+        backgroundColor: "transparent"
+      }}
+    >
+      <FlatList
+        //style={{ width: width }}
+        data={data}
+        renderItem={({ item }) => (
+          <Surface
+            style={{
+              margin: 3,
+              padding: 6,
+              backgroundColor: item.color,
+              //elevation: 3,
+              justifyContent: "center",
+              width: width - 16,
 
+              height: height / 9 //divided by the number of classes you have, to a max of like 8, have a space at the bottom
+            }}
+          >
+            <View
+              style={{ alignItems: "center", flexDirection: "row", flex: 1 }}
+            >
+              <View
+                style={{
+                  //backgroundColor: "blue",
+                  //height: 20,
+                  width: width - width / 2 - 21,
+                  justifyContent: "center",
+                  alignItems: "center"
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "left",
+                    //alignSelf: "stretch",
+                    color: "white",
+                    fontSize: responsiveFontSize(3)
+                  }}
+                >
+                  {item.name}
+                </Text>
+              </View>
+              <View
+                style={{
+                  //backgroundColor: "red",
+                  //height: 20,
+                  width: width - width / 2 - 20,
+                  flexDirection: "column",
+                  justifyContent: "center"
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "right",
+                    //alignSelf: "stretch",
+                    color: "white",
+                    fontSize: responsiveFontSize(2.6)
+                  }}
+                >
+                  {item.grade}
+                </Text>
+                <Text
+                  style={{
+                    textAlign: "right",
+                    //alignSelf: "stretch",
+                    color: "white",
+                    fontSize: responsiveFontSize(2.6)
+                  }}
+                >
+                  {item.letterGrade}
+                </Text>
+              </View>
+            </View>
+          </Surface>
+        )}
+        ItemSeparatorComponent={this.renderSeparator}
+        //ListHeaderComponent={this.renderHeader}
+        ListFooterComponent={this.renderFooter}
+        keyExtractor={item => item.name}
+      />
+    </List>
+  </SafeAreaView>
+);
+const settingsPage = () => {
+  <View
+    style={{ flex: 1, width: width - 9, justifyContent: "center" }}
+    tabLabel="Settings"
+  >
+    <SettingsList backgroundColor="transparent">
+      <SettingsList.Header
+        headerText="Settings"
+        headerStyle={{ color: "black" }}
+      />
+
+      <SettingsList.Item
+        itemWidth={50}
+        title="About/Credits"
+        onPress={() => Alert.alert("Go to credits website")}
+      />
+      <SettingsList.Item
+        itemWidth={50}
+        title="Help"
+        onPress={() => Alert.alert("Direct to help website")}
+      />
+      <SettingsList.Item
+        itemWidth={50}
+        title="Logout"
+        onPress={() =>
+          Alert.alert(
+            "Log out",
+            "Do you want to log out?",
+            [
+              {
+                text: "No",
+                onPress: () => console.log("Cancel Pressed")
+              },
+              {
+                text: "Yes",
+                onPress: () => this.props.navigation.navigate("Login")
+              }
+            ],
+            { cancelable: false }
+          )
+        }
+      />
+    </SettingsList>
+    {/*make is so the saved username and password are cleared when logout*/}
+  </View>;
+};
 const responsiveFontSize = f => {
   return Math.sqrt(height * height + width * width) * (f / 100);
 };
